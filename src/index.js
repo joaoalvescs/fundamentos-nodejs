76f8d61ca4b414f8c2bf5953b0af8014604d9b30
João Alves <joaoalves.cs@gmail.com>
2022-06-06 21:50:53 -0300

    Atualizar conta

--- src/index.js
+++ src/index.js
@@ -69,4 +69,19 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     return response.status(201).send()
 })
 
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body
+    const { customer } = request
+
+    customer.name = name
+
+    return response.status(201).send()
+})
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+
+    return response.json(customer)
+})
 app.listen(3333)
